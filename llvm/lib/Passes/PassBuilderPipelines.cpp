d6d1b81367979a372f8aad26fee7390f786c81da
Ryotaro Kasuga <kasuga.ryotaro@fujitsu.com>
2025-11-28 09:11:07 +0000

    enable loop-interchange

--- llvm/lib/Passes/PassBuilderPipelines.cpp
+++ llvm/lib/Passes/PassBuilderPipelines.cpp
@@ -1559,8 +1559,7 @@ PassBuilder::buildModuleOptimizationPipeline(OptimizationLevel Level,
   //        in the simplification pipeline.
   LPM.addPass(LoopDeletionPass());
 
-  if (PTO.LoopInterchange)
-    LPM.addPass(LoopInterchangePass());
+  LPM.addPass(LoopInterchangePass());
 
   OptimizePM.addPass(
       createFunctionToLoopPassAdaptor(std::move(LPM), /*UseMemorySSA=*/false));
